9ce1ebd2861a4dbace9838fcbced977400e433a8
ethHong <sukhyun9673@gmail.com>
2025-04-23 02:18:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -529,7 +529,7 @@ async def register(user: UserCreate):
 @app.post("/api/auth/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(form_data.username, form_data.password)
-    if not user or isinstance(user, bool):
+    if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
@@ -537,7 +537,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
+        data={"sub": user["email"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
